9edef80eafda834e521f69341d4f973c64390646
Daryn DeMoss <darynbrennondemoss@gmail.com>
2020-06-05 17:27:12 -0400

    Add project files.

---
+++ Chapter3Studio/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Chapter3Studio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the text you wnat to analyze: ");
+
+            string text = Console.ReadLine();
+
+            Dictionary<char, int> charCount = new Dictionary<char, int>();
+
+            foreach (char character in text)
+            {
+                if (charCount.ContainsKey(character))
+                {
+                    charCount(character) += 1;
+                }
+                else
+                {
+                    charCount.Add(character, 1);
+                }
+            }
+
+            foreach (KeyValuePair<char, int> count in charCount)
+            {
+                Console.WriteLine($"{count.Key} : {count.Value}");
+            }
+        }
+    }
+}
